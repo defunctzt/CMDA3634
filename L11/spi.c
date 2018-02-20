1bd399e8982905bd735a3cc4dde9bf69d985eec5
Zorian Thornton <zorian15@vt.edu>
2018-02-20 16:49:25 -0500

    Update for in class assignment 1

--- L11/spi.c
+++ L11/spi.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include "mpi.h"
+#include <time.h>
 
 int main(int argc, char **argv) {
 
+	MPI_Init(&argc, &argv);
+	int rank, size;
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
   //need running tallies
   long long int Ntotal;
   long long int Ncircle;
 
   //seed random number generator
   double seed = 1.0;
-  srand48(seed);
+  srand48(time(NULL));
+  
 
   for (long long int n=0; n<1000000000;n++) {
     //gererate two random numbers
@@ -25,9 +32,15 @@ int main(int argc, char **argv) {
     Ntotal++;
   }
 
-  double pi = 4.0*Ncircle/ (double) Ntotal;
+  MPI_Allreduce(&Ncircle, &Ntotal, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD); 
 
-  printf("Our estimate of pi is %f \n", pi);
+  if (rank == 0)
+  {
+ 	 double pi = 4.0*Ncircle/ (double) Ntotal;
+     printf("Our estimate of pi is %f \n", pi);
+  }
+
+  MPI_Finalize();
 
   return 0;
 }
