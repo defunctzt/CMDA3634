4ee7706d16508f7f144df5a5d6bccce2e78a9d7c
Zorian Thornton <zorian15@vt.edu>
2018-02-20 17:54:15 -0500

    Tried to change spi.c.... did not work out very well.

--- L11/spi.c
+++ L11/spi.c
@@ -2,23 +2,26 @@
 #include <stdlib.h>
 #include <math.h>
 #include "mpi.h"
-#include <time.h>
 
 int main(int argc, char **argv) {
-
+	MPI_Status status;
 	MPI_Init(&argc, &argv);
 	int rank, size;
+	double estimate, rep;
+	estimate = 0;
+	rep = 0;
+	double pi = 0;
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
-  //need running tallies
+
+//need running tallies
   long long int Ntotal;
   long long int Ncircle;
 
-  //seed random number generator
-  double seed = 1.0;
-  srand48(time(NULL));
-  
-
+  //seed random number generator.
+  srand48(rank);
+	  
+	
   for (long long int n=0; n<1000000000;n++) {
     //gererate two random numbers
     double rand1 = drand48(); //drand48 returns a number between 0 and 1
@@ -31,14 +34,22 @@ int main(int argc, char **argv) {
     if (sqrt(x*x+y*y)<=1) Ncircle++;
     Ntotal++;
   }
+	rep = Ncircle/(double) Ntotal;
+	MPI_Allreduce(&rep, &pi, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD); 
+	pi = pi/size;
 
-  MPI_Allreduce(&Ncircle, &Ntotal, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD); 
+	if (rank == 0 && Ntotal % 100 == 0)
+	{
+ 		 pi = 4.0*Ncircle/ (double) Ntotal;
+		 printf("Our estimate of pi is %f \n", pi);
+    }
+	 MPI_Barrier(MPI_COMM_WORLD);
 
-  if (rank == 0)
-  {
- 	 double pi = 4.0*Ncircle/ (double) Ntotal;
-     printf("Our estimate of pi is %f \n", pi);
-  }
+	 if (rank == 0)
+	 {
+    	 printf("Our estimate of pi is %f \n",4*pi);
+	 }
+
 
   MPI_Finalize();
 
